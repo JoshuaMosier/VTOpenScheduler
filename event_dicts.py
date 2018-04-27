9e2a03ea644d7927bb734463847ac3a6db5470ea
Josh Mosier <josh.r.mosier@gmail.com>
2018-04-27 12:02:20 -0400

    Added dictionary access methods

--- event_dicts.py
+++ event_dicts.py
@@ -1,15 +1,16 @@
 import copy
+import collections
 
 inputfile = open('Rooms by Building.txt')
 my_text = inputfile.readlines()
-buildings = {}
+build_dict = {}
 room = {}
 
 #Revsered order so it generates a room dict first
 for line in reversed(my_text):
 	#Isolate out buildings
 	if '    ' not in line:
-		buildings[line.strip()] = room
+		build_dict[line.strip()] = room
 		room = {}
 	else:
 		room[line.strip()] = line.strip()
@@ -23,7 +24,7 @@ days = {'mon': [], 'tues': [], 'wed': [], 'thurs': [], 'fri': [], 'sat': [], 'su
 currTime = {}
 
 #This gets a dictionary for classes
-class_dict = copy.deepcopy(buildings)
+class_dict = copy.deepcopy(build_dict)
 for line in reversed(my_text):
 	#Gets the current Building/Room Pair
 	if '    ' not in line:
@@ -60,7 +61,7 @@ days = {'mon': [], 'tues': [], 'wed': [], 'thurs': [], 'fri': [], 'sat': [], 'su
 currTime = {}
 
 #This gets a dictionary for events
-event_dict = copy.deepcopy(buildings)
+event_dict = copy.deepcopy(build_dict)
 for line in reversed(my_text):
 	#Gets the current Building/Room Pair
 	if '    ' not in line:
@@ -75,22 +76,92 @@ for line in reversed(my_text):
 	elif 'Events_' in line:
 		for key in days:
 			if key in line:
+				#2/15/18 - 4/26/18  Club Meetings  D: Young Americans for Liberty N: Andrew Letzkus aletzkus@vt.edu (571) 292 &nbsp;
 				class_info = []
-				removed_day = line.split('                    ',1)[1].strip()
-				date_range = removed_day.split('  ',1)[0]
-				details = removed_day.split('  ',1)[1]
+				removed_day = line.split('                    ',1)[1].strip().replace("&nbsp;","")
+				date_range = removed_day.split('  ',1)[0].strip()
+				details = removed_day.split('  ',1)[1].strip()
+				purpose = details.split('  ',1)[0].strip()
+				sub_details = details.split('  ',1)[1].strip()
+				organizer = sub_details.split('N:')[1].strip()
+				group = sub_details.split('N:',1)[0].strip().replace('D: ',"")
 				class_info.append(date_range)
-				class_info.append(details)
+				class_info.append(purpose)
+				class_info.append(group)
+				class_info.append(organizer)
+				#print(class_info)
 				days[key] = class_info
 
 # #Print out the keys for the building
-# for key in buildings.items():
+# for key in build_dict.items():
 # 	print(key)
 
-#Print out the keys for classes
-for key in class_dict.items():
-	print(key)
+# #Print out the keys for classes
+# for key in class_dict.items():
+# 	print(key)
 
 # #Print out the keys for events
 # for key in event_dict.items():
 # 	print(key)
+
+#Create Dictionary of Building codes --> Buidlings
+inputfile = open('Building Names.txt')
+my_text = inputfile.readlines()
+build_codes = {}
+
+for line in my_text:
+	split = line.split(' - ')
+	key = split[0].strip()
+	name = split[1].strip()
+	build_codes[key] = name
+
+
+#Get a list of rooms for a given building name or building key
+def get_room_list(building):
+	if building in build_dict.keys():
+		room_dict = build_dict[building]
+	elif build_codes[building] in build_dict.keys():
+		room_dict = build_dict[build_codes[building]]
+	else:
+		return None
+	room_list = []
+	for index,key in enumerate(collections.OrderedDict(sorted(room_dict.items()))):
+		room_list.append(key)
+	return room_list
+
+#Testing get_room_list
+#print(get_room_list("McBryde Hall"))
+
+#Get a list of rooms +/- 5 from a given building/room combo
+def get_nearby(building,room):
+	room_list = get_room_list(building)
+	index = room_list.index(room) if room in room_list else -1
+	if index != -1:
+		lower = index-5
+		upper = index+6
+		if lower  < 0:
+			lower = 0
+		if upper > len(room_list)-1:
+			upper = len(room_list)-1
+	return room_list[lower:upper]
+
+#Testing get_nearby
+#print(get_nearby("Cowgill Hall","300"))
+
+#Get the schedule for a list of rooms
+def get_schedules(building, rooms):
+	class_schedules = []
+	for room in rooms:
+		class_schedules.append(class_dict[building][room])
+	return class_schedules
+
+#Testing get_schedules
+building_name = "Cowgill Hall"
+room_number = "300"
+room_list = get_nearby(building_name,room_number)
+for index,schedule in enumerate(get_schedules(building_name,room_list)):
+	print("Building: " + building_name + " Room: " + room_list[index])
+	print(get_schedules(building_name,get_nearby(building_name,room_number))[index])
+	print()
+
+#Nice output for a specific schedule
