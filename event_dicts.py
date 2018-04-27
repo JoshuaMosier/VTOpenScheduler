3ec5e376788489d65348db04008079d2c88332fc
Josh Mosier <josh.r.mosier@gmail.com>
2018-04-27 01:23:37 -0400

    Finished dictionaries for buildings/classes/events

--- event_dicts.py
+++ event_dicts.py
@@ -1,3 +1,4 @@
+import copy
 
 inputfile = open('Rooms by Building.txt')
 my_text = inputfile.readlines()
@@ -21,15 +22,17 @@ my_text = inputfile.readlines()
 days = {'mon': [], 'tues': [], 'wed': [], 'thurs': [], 'fri': [], 'sat': [], 'sun': []}
 currTime = {}
 
+#This gets a dictionary for classes
+class_dict = copy.deepcopy(buildings)
 for line in reversed(my_text):
 	#Gets the current Building/Room Pair
 	if '    ' not in line:
 		currB = line.split(':')[0].strip()
 		currR = line.split(':')[1].strip()
-		buildings[currB][currR] = currTime
+		class_dict[currB][currR] = currTime
 		currTime = {}
 		#print(buildings[currB][currR])
-	elif 'Time' in line:
+	elif 'classTime' in line:
 		currTime[line.split('                ')[1].strip()] = days
 		days = {'mon': [], 'tues': [], 'wed': [], 'thurs': [], 'fri': [], 'sat': [], 'sun': []}
 	elif 'Classes' in line:
@@ -37,17 +40,57 @@ for line in reversed(my_text):
 			if key in line:
 				class_info = []
 				removed_day = line.split('                    ',1)[1].strip()
-				#Subject + Course number
-				removed_subj = removed_day.split("CRN: ")[0]
-				class_info.append(removed_subj)
-				#CRN
-				CRN = removed_day.split("CRN: ")[1][:5]
-				class_info.append(CRN)
-				#Professor
-				prof = removed_day.split("CRN: ")[1][5:]
-				class_info.append(prof)
+				sep_classes = removed_day.split('    ')
+				for info in sep_classes:
+					#Subject + Course number, CRN, Prof
+					removed_subj = info.split("CRN: ")[0].strip()
+					CRN = info.split("CRN: ")[1][:5].strip()
+					prof = info.split("CRN: ")[1][5:].strip()
+					class_info.append(removed_subj)
+					class_info.append(CRN)
+					class_info.append(prof)
 				days[key] = class_info
 
-#Print out the keys for the building
-for key in buildings.items():
+#reset stuff in case its breaking
+inputfile.close()
+
+inputfile = open('sorted_times.txt')
+my_text = inputfile.readlines()
+days = {'mon': [], 'tues': [], 'wed': [], 'thurs': [], 'fri': [], 'sat': [], 'sun': []}
+currTime = {}
+
+#This gets a dictionary for events
+event_dict = copy.deepcopy(buildings)
+for line in reversed(my_text):
+	#Gets the current Building/Room Pair
+	if '    ' not in line:
+		currB = line.split(':')[0].strip()
+		currR = line.split(':')[1].strip()
+		event_dict[currB][currR] = currTime
+		currTime = {}
+		#print(buildings[currB][currR])
+	elif 'eventTime' in line:
+		currTime[line.split('                ')[1].strip()] = days
+		days = {'mon': [], 'tues': [], 'wed': [], 'thurs': [], 'fri': [], 'sat': [], 'sun': []}
+	elif 'Events_' in line:
+		for key in days:
+			if key in line:
+				class_info = []
+				removed_day = line.split('                    ',1)[1].strip()
+				date_range = removed_day.split('  ',1)[0]
+				details = removed_day.split('  ',1)[1]
+				class_info.append(date_range)
+				class_info.append(details)
+				days[key] = class_info
+
+# #Print out the keys for the building
+# for key in buildings.items():
+# 	print(key)
+
+#Print out the keys for classes
+for key in class_dict.items():
 	print(key)
+
+# #Print out the keys for events
+# for key in event_dict.items():
+# 	print(key)
